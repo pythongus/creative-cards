0209cac376d3cc6a612a2758355637df3d61ba6d
Gustavo Garcia <pythongus@gmail.com>
2020-07-20 05:42:10 +1000

    Removed unused import

--- src/App.vue
+++ src/App.vue
@@ -22,7 +22,6 @@
 </template>
 
 <script>
-import FirebaseConfig from './firebaseConfig.js';
 import Header from './components/Header.vue';
 import CardFront from './components/card/CardFront.vue';
 import CardBack from './components/card/CardBack.vue';
